84a3026d88b8eb8e8f35b43ae73d5eb973c35b5c
Drichdev <rich10dev@gmail.com>
2023-12-23 21:38:22 +0000

    poke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: [],
+    <h1> listes de pokemon </h1>
+  `
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ng-poke';
+  pokeListe = POKEMONS;
+  ngOnInit() {
+    console.table(this.pokeListe);
+    this.selectPoke(this.pokeListe[1]);
+  }
+  selectPoke(pokemon: Pokemon){
+    console.log(`Le nom selectionné est ${pokemon.name}`)
+  }
 }
+
